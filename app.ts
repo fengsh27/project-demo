e64a185e93c058206a616b8398f4fb636d8582f8
fengsh <shaohong.feng.78@gmail.com>
2023-01-30 12:55:46 -0500

    remove log

--- app.ts
+++ app.ts
@@ -4,13 +4,11 @@ const app = express();
 const port = 9003;
 
 app.get("/", (req:any, res:any) => {
-  console.log(`end point: "/"`);
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>Welcome to demo project!</h1>');
   res.end();
 });
 app.get("/about", (req:any, res:any) => {
-  console.log(`end point: "/about"`);
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>About us page</h1>');
   res.end();
