3d292c843b2dfa0b026a5cb5c0fd7997a5fc8b8b
fengsh27 <123769183+fengsh27@users.noreply.github.com>
2023-02-09 15:45:13 -0500

    merge Release/rabbit to main (#2)
    * add hello route
    
    * modify Dockerfile
    
    * add tests
    
    * expose port 80
    
    * test
    
    * test
    
    * add endpoint /visit
    
    * fix a typo
    
    * fix a typo
    
    ---------
    
    Co-authored-by: fengsh <shaohong.feng.78@gmail.com>

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const app = express();
-const port = 9003;
+const port = 80;
 
 app.get("/", (req:any, res:any) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
@@ -16,8 +16,14 @@ app.get("/about", (req:any, res:any) => {
 app.get("/hello", (req: any, res: any) => {
   console.log(`end point: "/hello"`);
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1>Hello World!</h1>');
+  res.write('<h1>Hello World!</h1><div>You are witnessing history!</div>');
 });
+app.get("/visit", (req: any, res: any) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<h1>You are an visitor!</h1>');
+  res.end();
+});
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
