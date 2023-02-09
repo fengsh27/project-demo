a9060137defd649b1059ed2cd72e7bea64719823
fengsh <shaohong.feng.78@gmail.com>
2023-02-09 14:17:52 -0500

    fix a typo

--- app.ts
+++ app.ts
@@ -17,10 +17,11 @@ app.get("/hello", (req: any, res: any) => {
   console.log(`end point: "/hello"`);
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>Hello World!</h1><div>You are witnessing history!</div>');
+});
 app.get("/visit", (req: any, res: any) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1>You are an visitor!</h1>);
-});
+  res.write('<h1>You are an visitor!</h1>');
+  res.end();
 });
 
 app.listen(port, () => {
