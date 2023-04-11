8cd9e677623328601e2b6486e0acf9e2d6dc0ba0
fengsh27 <123769183+fengsh27@users.noreply.github.com>
2023-04-11 16:03:41 -0400

    Merge: merge main to release/rabbit (#6)
    * merge Release/rabbit to main (#2)
    
    * add hello route
    
    * modify Dockerfile
    
    * add tests
    
    * expose port 80
    
    * test
    
    * test
    
    * add endpoint /visit
    
    * fix a typo
    
    * fix a typo
    
    ---------
    
    Co-authored-by: fengsh <shaohong.feng.78@gmail.com>
    
    * add azure instrument test code (#3)
    
    * add instrument test code
    
    * print error of initiating telemetry briefly
    
    ---------
    
    Co-authored-by: fengsh <shaohong.feng.78@gmail.com>

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const app = express();
+require("./initTelemetry");
 const port = 80;
 
 app.get("/", (req:any, res:any) => {
@@ -9,6 +10,7 @@ app.get("/", (req:any, res:any) => {
   res.end();
 });
 app.get("/about", (req:any, res:any) => {
+  console.log(`end point: "/about"`);
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>About us page</h1>');
   res.end();
@@ -17,8 +19,10 @@ app.get("/hello", (req: any, res: any) => {
   console.log(`end point: "/hello"`);
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>Hello World!</h1><div>You are witnessing history!</div>');
+  res.end();
 });
 app.get("/visit", (req: any, res: any) => {
+  console.log(`end point: "/visit"`);
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>You are an visitor!</h1>');
   res.end();
